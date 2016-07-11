a11a62aa10af43e2c1774f8568e67016a6f8337f
sheafk <sheafureyking@gmail.com>
2016-07-11 08:54:13 -0400

    Completed lab.

--- ViewLifeCycle/ViewLifeCycle/ViewController.swift
+++ ViewLifeCycle/ViewLifeCycle/ViewController.swift
@@ -5,18 +5,18 @@ import UIKit
 
 
 class ViewController: UIViewController {
-    override func viewDidLoad() {
+    override func viewDidLoad() { //Gets called first and only called once
         super.viewDidLoad()
         print("viewDidLoad() called")
         view.backgroundColor = UIColor.randomColor()
     }
-
-    override func viewWillAppear(animated: Bool) {
+//view is an instance variable
+    override func viewWillAppear(animated: Bool) {//Gets called second and anytime the view appears
         super.viewWillAppear(animated)
         print("viewWillAppear(_:) called")
     }
 
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(animated: Bool) {//This is called after the view appears. Users will see anything change in this method happen in action.
         super.viewDidAppear(animated)
         print("viewDidAppear(_:) called")
     }
